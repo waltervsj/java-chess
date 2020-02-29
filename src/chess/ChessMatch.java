681bea1c849d27e37464aef1ed5cd53e68733529
Walter Vieira <waltervsj@gmail.com>
2020-02-29 16:50:01 -0300

    King possible moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -74,7 +74,7 @@ public class ChessMatch {
         placeNewPiece('d', 2, new Rook(board, Color.WHITE));
         placeNewPiece('e', 2, new Rook(board, Color.WHITE));
         placeNewPiece('e', 1, new Rook(board, Color.WHITE));
-        placeNewPiece('d', 1, new King(board, Color.WHITE));
+        placeNewPiece('d', 4, new King(board, Color.WHITE));
 
         placeNewPiece('c', 7, new Rook(board, Color.BLACK));
         placeNewPiece('c', 8, new Rook(board, Color.BLACK));
--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -17,9 +18,56 @@ public class King extends ChessPiece {
 
 	@Override
 	public boolean[][] possibleMoves() {
-		boolean[][] possibleMovesMatrix = new boolean[this.getBoard().getRows()][this.getBoard().getColumns()];
-		return possibleMovesMatrix;
+		boolean[][] matPossibleMoves = new boolean[this.getBoard().getRows()][this.getBoard().getColumns()];
+
+		Position scanPos = new Position(0, 0);
+
+		// North
+		scanPos.setValues(this.position.getRow() - 1, this.position.getColumn());
+		if (getBoard().positionExists(scanPos) && canMove(scanPos))
+		matPossibleMoves[scanPos.getRow()][scanPos.getColumn()] = true;
+
+		// NorthEast
+		scanPos.setValues(this.position.getRow() - 1, this.position.getColumn() + 1);
+		if (getBoard().positionExists(scanPos) && canMove(scanPos))
+			matPossibleMoves[scanPos.getRow()][scanPos.getColumn()] = true;
+		 
+
+		// East
+		scanPos.setValues(this.position.getRow(), this.position.getColumn() + 1);
+		if (getBoard().positionExists(scanPos) && canMove(scanPos))
+			matPossibleMoves[scanPos.getRow()][scanPos.getColumn()] = true;
+
+		// SouthEast
+		scanPos.setValues(this.position.getRow() + 1, this.position.getColumn() - 1);
+		if (getBoard().positionExists(scanPos) && canMove(scanPos))
+			matPossibleMoves[scanPos.getRow()][scanPos.getColumn()] = true;
+
+		// South
+		scanPos.setValues(this.position.getRow() + 1, this.position.getColumn());
+		if (getBoard().positionExists(scanPos) && canMove(scanPos))
+			matPossibleMoves[scanPos.getRow()][scanPos.getColumn()] = true;
+
+		// SouthWest
+		scanPos.setValues(this.position.getRow() + 1, this.position.getColumn() + 1);
+		if (getBoard().positionExists(scanPos) && canMove(scanPos))
+			matPossibleMoves[scanPos.getRow()][scanPos.getColumn()] = true;
+
+		// West
+		scanPos.setValues(this.position.getRow(), this.position.getColumn() - 1);
+		if (getBoard().positionExists(scanPos) && canMove(scanPos))
+			matPossibleMoves[scanPos.getRow()][scanPos.getColumn()] = true;
+
+		// NorthWest
+		scanPos.setValues(this.position.getRow() - 1, this.position.getColumn() - 1);
+		if (getBoard().positionExists(scanPos) && canMove(scanPos))
+			matPossibleMoves[scanPos.getRow()][scanPos.getColumn()] = true;
+
+		return matPossibleMoves;
 	}
-	
-	
+
+	private boolean canMove(Position position) {
+		ChessPiece piece = (ChessPiece) getBoard().piece(position);
+		return piece == null || (piece != null && piece.getColor() != getColor());
+	}
 }
